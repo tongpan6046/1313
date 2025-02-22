b48f6d14531c97c253c86cc6d0e4acb83f9777ff
tongpan6046 <tongpan6046@icloud.com>
2025-02-22 19:52:36 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open("13CardsCache").then((cache) => {
+            return cache.addAll([
+                "index.html",
+                "styles.css",
+                "app.js",
+                "manifest.json"
+            ]);
+        })
+    );
+});
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
